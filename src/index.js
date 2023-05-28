9d68831025f6793d1a298cb97fb395485df4630e
Daniel Guerrero <danielgdevil@gmail.com>
2023-05-28 17:00:31 -0500

    Changes

--- src/index.js
+++ src/index.js
@@ -30,7 +30,6 @@ app.use((req,res,next) => {
 //Routes
 app.use(require('./routes/index'));
 app.use(require('./routes/authentication'));
-app.use('/links',require('./routes/links'));
 app.use(require('./routes/certifications'));
 
 //Publics
--- src/routes/certifications.js
+++ src/routes/certifications.js
@@ -7,12 +7,4 @@ router.post('/download', generarCertificado, (req, res) => {
     const {nombre,id,tipo} = req.body;
 })
 
-module.exports =  router
-const express = require('express')
-const router = express.Router()
-
-router.get('/Certificados', (req, res) => {
-    res.render('Certificados/Certificados_mod.hbs')
-}) 
-
 module.exports = router;
