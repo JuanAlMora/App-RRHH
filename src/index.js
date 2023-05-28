77a48a83e9081f00e6b16e1c807e047091f5d57c
Juan Alejandro Mora <49284628+JuanAlMora@users.noreply.github.com>
2023-05-28 16:37:09 -0500

    Nuevo nombre Certifications

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ app.use((req,res,next) => {
 app.use(require('./routes/index'));
 app.use(require('./routes/authentication'));
 app.use('/links',require('./routes/links'));
-app.use(require('./routes/certificados'));
+app.use(require('./routes/certifications'));
 
 //Publics
 app.use(express.static(path.join(__dirname,'public')));
--- src/routes/certifications.js
+++ src/routes/certifications.js
@@ -2,20 +2,9 @@ const express = require('express')
 const router = express.Router()
 const { certificados, generarCertificado } = require('../controllers/certificadoController')
 
-// router.get('/certificados', (req, res) => {
-//     res.render('vistasCertificados/moduloCertificaciones.hbs')
-// }) 
-
 router.get('/certificados', certificados);
 router.post('/download', generarCertificado, (req, res) => {
     const {nombre,id,tipo} = req.body;
-    const newCertificado = {
-        nombre,
-        id,
-        tipo
-    };
-});
-
-
+})
 
 module.exports =  router
