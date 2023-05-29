4b8cca4df1cdc79126cb4c8814b5d4b0063155af
Daniel Guerrero <danielgdevil@gmail.com>
2023-05-28 22:29:29 -0500

    Update_v0.1.5
    Ultimas actualizaciones para la sección mas crítica

--- src/routes/list_emp_cv.js
+++ src/routes/list_emp_cv.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const connection = require('../database')
 
 router.get('/ListaEmpleados', (req, res) => {
-    res.render('recursoHumano/empleados/listaEmpleados.hbs')
+    connection.query('SELECT id, documento, nombre, correo, telefono FROM recurso_humano WHERE empleado = 1', (err, rows) => {
+        if (err) {
+            res.json(err)
+        }
+        res.render('recursoHumano/empleados/listaEmpleados.hbs', {rows})
     })
+})
 
 module.exports = router;
